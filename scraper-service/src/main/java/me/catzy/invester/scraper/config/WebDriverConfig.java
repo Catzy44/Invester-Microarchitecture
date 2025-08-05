c3d82f28b496c2a27c160b21744887ed11ad5c35
catzy44 <przemowi2002@gmail.com>
2025-08-05 17:18:38 +0200

    chrome loc

--- scraper-service/src/main/java/me/catzy/invester/scraper/config/WebDriverConfig.java
+++ scraper-service/src/main/java/me/catzy/invester/scraper/config/WebDriverConfig.java
@@ -3,11 +3,15 @@ package me.catzy.invester.scraper.config;
 import java.util.Collections;
 
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebDriverConfig {
+	@Value("${chrome.location}")
+    private String chromeBinaryLocation;
+	
 	@SuppressWarnings("deprecation")
 	@Bean 
 	public ChromeOptions chromeOptions() {
@@ -16,6 +20,7 @@ public class WebDriverConfig {
 	    options.setExperimentalOption("useAutomationExtension", false);
 	    options.addArguments("--disable-blink-features=AutomationControlled");
 	    options.setHeadless(false);
+	    options.setBinary(chromeBinaryLocation);
 	    String modernUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
 	    options.addArguments("--user-agent=" + modernUserAgent);
 	    return options;
