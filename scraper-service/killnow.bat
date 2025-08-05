cfaf7a30e82075fe3aa2641c5697c02b101d9bf0
catzy44 <przemowi2002@gmail.com>
2025-08-05 18:35:33 +0200

    A

--- scraper-service/killnow.bat
+++ scraper-service/killnow.bat
@@ -1,3 +1,6 @@
-for /f "tokens=2 delims=," %%i in ('tasklist /v /fo csv ^| findstr /i "INVESTER SCRAPER MICROSERVICE"') do taskkill /pid %%i /f
+for /f "tokens=2 delims=," %%i in ('tasklist /v /fo csv ^| findstr /i "INVESTER SCRAPER MICROSERVICE"') do (
+  echo Killing PID %%i
+  taskkill /pid %%i /f
+)
 taskkill /f /im undetected_chromedriver.exe
 taskkill /f /im chrome.exe
