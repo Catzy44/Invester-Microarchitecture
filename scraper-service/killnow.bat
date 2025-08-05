65b526e23887c21ede74954891845805b0683700
catzy44 <przemowi2002@gmail.com>
2025-08-05 18:51:17 +0200

    QQQQ

--- scraper-service/killnow.bat
+++ scraper-service/killnow.bat
@@ -3,5 +3,13 @@ for /f "tokens=2 delims=," %%i in ('tasklist /v /fo csv ^| findstr /i "INVESTER 
   echo Killing PID %%i
   taskkill /pid %%i /f
 )
+for /f "tokens=2 delims=," %%i in ('tasklist /v /fo csv ^| findstr /i "INVESTER SCRAPER MICROSERVICE"') do (
+  echo Killing PID %%i
+  taskkill /pid %%i /f
+)
+for /f "tokens=2 delims=," %%i in ('tasklist /v /fo csv ^| findstr /i "INVESTER SCRAPER MICROSERVICE"') do (
+  echo Killing PID %%i
+  taskkill /pid %%i /f
+)
 taskkill /f /im undetected_chromedriver.exe
 taskkill /f /im chrome.exe
