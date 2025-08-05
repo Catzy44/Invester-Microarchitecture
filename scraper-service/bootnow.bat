7dc8ad25aab6c6cb434a8008ccfc9a3bfe098fdb
catzy44 <przemowi2002@gmail.com>
2025-08-05 17:28:37 +0200

    ac

--- scraper-service/bootnow.bat
+++ scraper-service/bootnow.bat
@@ -2,5 +2,6 @@
 cd /d %~dp0
 for /f "tokens=2 delims=," %%i in ('tasklist /v /fo csv ^| findstr /i "INVESTER SCRAPER MICROSERVICE"') do taskkill /pid %%i /f
 title INVESTER SCRAPER MICROSERVICE
+echo 
 call gradlew.bat bootRun --console plain --args='--spring.config.location=classpath:/application.properties'
 pause
