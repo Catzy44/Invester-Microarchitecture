42901bc6e4baf8272e4a083cc78c59129a69d401
catzy44 <przemowi2002@gmail.com>
2025-08-05 16:43:39 +0200

    A

--- scraper-service/bootnow.bat
+++ scraper-service/bootnow.bat
@@ -1,4 +1,5 @@
-@echo off
+@echo on
+cd /d %~dp0
 title INVESTER SCRAPER MICROSERVICE
 call gradlew.bat bootRun --console plain --args='--spring.config.location=classpath:/application.properties'
 pause
