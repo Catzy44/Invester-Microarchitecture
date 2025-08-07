47ca15854e237ebcf1941cef32bb66094c49a2b3
catzy44 <przemowi2002@gmail.com>
2025-08-07 10:42:56 +0200

    process man fixes

--- scraper-service/bootnow.bat
+++ scraper-service/bootnow.bat
@@ -2,4 +2,3 @@
 cd /d %~dp0
 title INVESTER SCRAPER MICROSERVICE
 call gradlew.bat bootRun --console plain --args='--spring.config.location=classpath:/application.properties'
-pause
--- scraper-service/killnow.bat
+++ scraper-service/killnow.bat
@@ -1,15 +1,13 @@
 @echo off
+
+set /p PID=<proc.pid
+taskkill /PID %PID% /F
+
 for /f "tokens=2 delims=," %%i in ('tasklist /v /fo csv ^| findstr /i "INVESTER SCRAPER MICROSERVICE"') do (
   echo Killing PID %%i
   taskkill /pid %%i /f
 )
-for /f "tokens=2 delims=," %%i in ('tasklist /v /fo csv ^| findstr /i "INVESTER SCRAPER MICROSERVICE"') do (
-  echo Killing PID %%i
-  taskkill /pid %%i /f
-)
-for /f "tokens=2 delims=," %%i in ('tasklist /v /fo csv ^| findstr /i "INVESTER SCRAPER MICROSERVICE"') do (
-  echo Killing PID %%i
-  taskkill /pid %%i /f
-)
+
 taskkill /f /im undetected_chromedriver.exe
+
 taskkill /f /im chrome.exe
