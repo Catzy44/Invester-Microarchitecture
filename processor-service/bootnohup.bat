ff34e56c6f9084128ff1057f0bf249ab05f031a0
catzy44 <przemowi2002@gmail.com>
2025-09-04 14:27:07 +0200

    fix fu ci/cd

--- processor-service/bootnohup.bat
+++ processor-service/bootnohup.bat
@@ -1,7 +1,12 @@
 @echo off
+
+:: important - cd script location directory
 cd /d %~dp0
 
-schtasks /create /tn Processor /tr "\"%CD%\bootnow.bat\"" /sc once /st 00:00 /rl limited
+:: fetch current desktop user and save into CURR variable
+for /f "usebackq delims=" %%U in (`powershell -NoP -C "(Get-CimInstance Win32_ComputerSystem).UserName"`) do set CURR=%%U
+
+schtasks /create /tn Processor /tr "\"%CD%\bootnow.bat\"" /sc once /st 00:00 /rl limited /RU %CURR% /it
 schtasks /run /tn Processor
 timeout /t 2
 schtasks /delete /tn Processor /f
--- processor-service/bootnow.bat
+++ processor-service/bootnow.bat
@@ -1,4 +1,8 @@
 @echo off
+
+:: important - cd script location directory
+cd /d %~dp0
+
 title INVESTER PROCESSOR MICROSERVICE
 call gradlew.bat bootRun --console plain --args='--spring.config.location=classpath:/application.properties'
 pause
