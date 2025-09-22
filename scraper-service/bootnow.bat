bd014f1a75e80cdcca22d9ead66020bd31fc8b3f
catzy44 <przemowi2002@gmail.com>
2025-09-22 19:54:38 +0200

    fixed scraper terminal colors and formatting

--- scraper-service/bootnow.bat
+++ scraper-service/bootnow.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
 title INVESTER SCRAPER MICROSERVICE
-call gradlew.bat bootRun --args='--spring.config.location=classpath:/application.properties'
+call gradlew.bat bootRun --console plain --args='--spring.config.location=classpath:/application.properties'
