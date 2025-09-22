1355b1f0f6ef1121d500ddcb7d63aea15e02a6f3
catzy44 <przemowi2002@gmail.com>
2025-09-22 17:35:37 +0200

    gradle updated to 9.1.0

--- scraper-service/src/main/java/me/catzy/invester/scraper/ScraperApplication.java
+++ scraper-service/src/main/java/me/catzy/invester/scraper/ScraperApplication.java
@@ -4,11 +4,10 @@ import java.io.File;
 import java.io.FileWriter;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
+//@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 @EnableScheduling
 //@EnableCaching
 public class ScraperApplication {
--- scraper-service/src/main/java/me/catzy/invester/scraper/config/WebDriverConfig.java
+++ scraper-service/src/main/java/me/catzy/invester/scraper/config/WebDriverConfig.java
@@ -12,14 +12,13 @@ public class WebDriverConfig {
 	@Value("${chrome.location}")
     private String chromeBinaryLocation;
 	
-	@SuppressWarnings("deprecation")
 	@Bean 
 	public ChromeOptions chromeOptions() {
 		ChromeOptions options = new ChromeOptions();
 	    options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
 	    options.setExperimentalOption("useAutomationExtension", false);
 	    options.addArguments("--disable-blink-features=AutomationControlled");
-	    options.setHeadless(false);
+	    //options.setHeadless(false);
 	    options.setBinary(chromeBinaryLocation);
 	    String modernUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
 	    options.addArguments("--user-agent=" + modernUserAgent);
